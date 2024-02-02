5e299d315b11dfb402a357d85944d5036d311def
Edvard Thörnros <edvard.thornros@gmail.com>
2024-02-02 22:28:17 +0100

    Input

--- main.lua
+++ main.lua
@@ -7,7 +7,13 @@ function love.load()
 end
 
 function love.update(dt)
-  state = game.update(dt)(state)
+  local input =
+    { left = love.keyboard.isDown("left") or love.keyboard.isDown("a")
+    , right = love.keyboard.isDown("right") or love.keyboard.isDown("d")
+    , up = love.keyboard.isDown("up") or love.keyboard.isDown("w")
+    , down = love.keyboard.isDown("down") or love.keyboard.isDown("s")
+    }
+  state = game.update(dt)(input)(state)
   print(state)
 end
 
