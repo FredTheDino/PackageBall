2972d3bf630108c8b6cb21376d166c86608031eb
Erik Mattfolk <erik.mattfolk@gmail.com>
2024-02-03 11:53:26 +0100

    Fix

--- main.lua
+++ main.lua
@@ -1,24 +1,23 @@
-local game = require "game"
+local game = require("game")
 
 local last_modified = 0
 local state = nil
 function love.load()
-  state = game.init(0)
+	state = game.init(0)
 end
 
 function love.update(dt)
-  package.loaded["game"] = nil
-  game = require "game"
-  local input =
-    { left = love.keyboard.isDown("left") or love.keyboard.isDown("a")
-    , right = love.keyboard.isDown("right") or love.keyboard.isDown("d")
-    , up = love.keyboard.isDown("up") or love.keyboard.isDown("w")
-    , down = love.keyboard.isDown("down") or love.keyboard.isDown("s")
-    }
-  state = game.update(dt)(input)(state)
-  print(state)
+	package.loaded["game"] = nil
+	game = require("game")
+	local input = {
+		left = love.keyboard.isDown("left") or love.keyboard.isDown("a"),
+		right = love.keyboard.isDown("right") or love.keyboard.isDown("d"),
+		up = love.keyboard.isDown("up") or love.keyboard.isDown("w"),
+		down = love.keyboard.isDown("down") or love.keyboard.isDown("s"),
+	}
+	state = game.update(dt)(input)(state)
 end
 
 function love.draw()
-  game.draw(state)()
+	game.draw(state)()
 end
